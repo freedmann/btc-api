3f6695948c9c824275b8dad24557a8b6687b5f68
freedmann <da@MacBook-Pro-da.local>
2023-02-13 22:57:54 +0300

    edit

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Tests\Unit\CreatesApplication;
 
 abstract class TestCase extends BaseTestCase
 {
