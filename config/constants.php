31d472780be16b159f3a2de15bc939a883ae4016
freedmann <da@MacBook-Pro-da.local>
2023-02-13 21:40:55 +0300

    initial

---
+++ config/constants.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'convert_commission' => 2,
+    'floor_convert_value'  => 0.1,
+    'api_key' => 'S7dh-S8_2jdh76d35tsGDfs-sj_jdSD_88SdKj7-2d7G-2LMv_78AuI-3$31J7F',
+    'btc_ticker_url' => 'https://blockchain.info/ticker',
+];
+?>
